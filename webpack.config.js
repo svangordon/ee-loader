7ada006c44cb6ef8376ad39a198caddacc2ae59e
Stephen Van Gordon <stephen.van.gordon@gmail.com>
2017-07-19 15:15:49 -0700

    change output directory

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,9 @@
 var path = require('path');
-console.log("=== dirname ===", __dirname, path.resolve(__dirname, "node_modules"))
 module.exports = {
   entry: "./index.js",
   output: {
     filename: 'script.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve("./", 'dist')
   },
   module: {
     rules: [
